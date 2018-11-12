60958dcf26966517cacaca017ddb900b1767eff3
Xavier Lavayssière <xavier.lavayssiere@gmail.com>
2018-11-12 12:20:24 +0100

    agencies iteration

---
+++ data.js
@@ -0,0 +1,51 @@
+let agencies = {
+  "IMF" : {
+    "name": "The International Monetary Fund",
+    "description": "IMF facilitates international monetary cooperation and financial stability, and provides a permanent forum for consultation, advice, and assistance on financial issues",
+    "headquarters":"Washington D.C., USA",
+    "logo":"logos/IMF.png"
+  },
+  "ILO" : {
+    "name": "The International Labor Organization",
+    "description": "ILO formulates policies and programs to improve working conditions and employment opportunities, and defines international labor standards as guidelines for governments.",
+    "headquarters":"Geneva, Switerland",
+    "logo": "logos/ILO.png"
+  },
+   "ECLAC" : {
+    "name": "United Nations Economic Commission for Latin America and the Caribbean",
+    "description": "ECLAC is a United Nations regional commission to encourage economic cooperation",
+    "headquarters":"Santiago, Chile",
+    "logo": "logos/ECLAC.png"
+  },
+   "ECA" : {
+    "name": "United Nations Economic Commission for Africa",
+    "description": "The United Nations Economic Commission for Africa (UNECA or ECA) was established in 1958 to encourage economic cooperation among its member states (the nations of the African continent) following a recommendation of the United Nations General Assembly",
+    "headquarters":"Addis Abeba, Ethiopia"
+  },
+  "ESCAP" : {
+    "name": "United Nations Economic and Social Commission for Asia and the Pacific",
+    "description": "ESCAP iP is one of the five regional commissions of the United Nations Economic and Social Council, under the administrative direction of the United Nations headquarters. It was established to encourage economic cooperation among its member states",
+    "headquarters":"Bangkok, Thailand"
+  },
+  "ESCWA" : {
+    "name": "United Nations Economic and Social Commission for West Asia ",
+    "description": "ESCWA is one of five regional commissions under the administrative direction of the United Nations Economic and Social Council. ESCWA promotes economic and social development of Western Asia through regional and subregional cooperation and integration. ",
+    "headquarters":"Beirut, Lebanon"
+  },
+  "FAO" : {
+    "name": "Food and Agricultural Organization",
+    "description": "is a specialized agency of the United Nations that leads international efforts to defeat hunger. Serving both developed and developing countries, FAO acts as a neutral forum where all nations meet as equals to negotiate arguments and debate policy.",
+    "headquarters":"Quebec, Canada"
+  },
+  "IAEA" : {
+    "name": "International Atomic Energy Agency",
+    "description": "IAEA s an international organization that seeks to promote the peaceful use of nuclear energy, and to inhibit its use for any military purpose, including nuclear weapons.",
+    "headquarters":"Vienna, Austria"
+  },
+  "IFAD" : {
+    "name": "International Fund for Agricultural Development",
+    "description": "IFAD is an international financial institution and a specialised agency of the United Nations dedicated to eradicating poverty and hunger in rural areas of developing countries.",
+    "headquarters":"Roma, Italy"
+  }
+
+}
